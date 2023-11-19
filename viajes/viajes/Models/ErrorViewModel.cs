36917130e2868355021b30f6eace834eeecbbb5d
JordanZ <jordan.zelayahs@gmail.com>
2023-11-19 14:27:17 -0600

    First commit

---
+++ viajes/viajes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace viajes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
